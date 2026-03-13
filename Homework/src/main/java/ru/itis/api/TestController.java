637005e92e879b510a1ba865f0d35267abc4f104
Angelina Balonina <balonina.angelina@yandex.ru>
2026-03-13 20:19:56 +0300

    Third homework+

---
+++ Homework/src/main/java/ru/itis/api/TestController.java
@@ -0,0 +1,29 @@
+package ru.itis.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.itis.service.UserService;
+
+@Controller
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class TestController {
+    private final UserService userService;
+
+    @RequestMapping(path = "/users", method = RequestMethod.GET)
+    public String getUsersPage() {
+        return null;
+    }
+
+    @GetMapping(path = "/users/v2")
+    public String getUsersPageV2() {
+        return "index";
+    }
+
+    @GetMapping("/users/{id}")
+    public String getUserById(@PathVariable Long id,
+                              @RequestParam("age") Integer age) {
+        return null;
+    }
+}
